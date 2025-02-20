d2fdf1c77e785b7c9a79e213f110b3969cf56f15
sravyamallapu <124546823+sravyamallapu@users.noreply.github.com>
2025-02-20 11:25:50 +0530

    Create 20 empty plot.R

---
+++ 20 empty plot.R
@@ -0,0 +1,8 @@
+# Create an empty plot with specified axes limits
+plot(NA,                 # No data to plot
+     xlim = c(0, 10),    # X-axis limits from 0 to 10
+     ylim = c(0, 20),    # Y-axis limits from 0 to 20
+     xlab = "X-axis",    # Label for the X-axis
+     ylab = "Y-axis",    # Label for the Y-axis
+     main = "Empty Plot with Specified Axes Limits",  # Title of the plot
+     type = "n")         # Type "n" for no plotting
